5a99f93b3eebc698811b2fa493fd29cb8602bf6e
Jens Finkhaeuser <jens@finkhaeuser.de>
2015-06-02 16:32:00 +0200

    Add charset awareness & transcoding to parsing JSON.

--- lib/faraday_json/parse_json.rb
+++ lib/faraday_json/parse_json.rb
@@ -5,10 +5,15 @@
 # Copyright (c) 2015 spriteCloud B.V. and other FaradayJSON contributors.
 # All rights reserved.
 #
+
+require 'faraday_json/encoding'
+
 module FaradayJSON
   # Public: Parse response bodies as JSON.
   class ParseJson < Faraday::Middleware
     CONTENT_TYPE = 'Content-Type'.freeze
+
+    include ::FaradayJSON::Encoding
 
     dependency do
       require 'json' unless defined?(::JSON)
@@ -30,22 +35,46 @@ module FaradayJSON
 
     def process_response(env)
       env[:raw_body] = env[:body] if preserve_raw?(env)
-      env[:body] = parse(env[:body])
-    end
+      body = env[:body]
 
-    def parse(body)
+      # Body will be in an unknown encoding. Use charset field to coerce it to
+      # internal UTF-8.
+      charset = response_charset(env)
+
+      # We must ensure we're interpreting the body as the right charset. First,
+      # strip the BOM (if any).
+      body = strip_bom(body, charset, { 'default_encoding' => 'us-ascii' })
+
+      # Transcode to UTF-8
+      body = to_utf8(body, charset, { 'force_input_charset' => true })
+
+      # Now that's done, parse the JSON.
+      ret = nil
       begin
-        ::JSON.parse(body) unless body.strip.empty?
+        ret = ::JSON.parse(body) unless body.strip.empty?
       rescue StandardError, SyntaxError => err
         raise err if err.is_a? SyntaxError and err.class.name != 'Psych::SyntaxError'
         raise Faraday::Error::ParsingError, err
       end
+      env[:body] = ret
     end
 
     def response_type(env)
       type = env[:response_headers][CONTENT_TYPE].to_s
       type = type.split(';', 2).first if type.index(';')
       type
+    end
+
+    def response_charset(env)
+      header = env[:response_headers][CONTENT_TYPE].to_s
+      if header.index(';')
+        header.split(';').each do |part|
+          if part.index('charset=')
+            return part.split('charset=', 2).last
+          end
+        end
+      end
+      return nil
     end
 
     def process_response_type?(type)
